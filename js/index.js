01ad21a00a9f2635ea6632d15dc2d500e84a59f6
unknown <dantim1974@hotmail.com>
2024-01-08 12:50:18 +0100

    secondary nav menu screen lg et +

--- js/index.js
+++ js/index.js
@@ -50,15 +50,66 @@ panier.addEventListener('click', modalPanierVideVisible);
 let cross = document.querySelector('.cross');
 let modalNewsletter = document.querySelector('.newsletter-modal');
 
-let modalNewsletterTimer = () => {
-    modalNewsletter.classList.add('newsletter-modal-visible');
-}
-setTimeout(()=> {
-    modalNewsletterTimer();
-}, 5000);
+// let modalNewsletterTimer = () => {
+//     modalNewsletter.classList.add('newsletter-modal-visible');
+// }
+// setTimeout(()=> {
+//     modalNewsletterTimer();
+// }, 5000);
 
 let closeNewsletterModal = () => {
     modalNewsletter.classList.remove('newsletter-modal-visible');
 };
 cross.addEventListener('click', closeNewsletterModal);
 
+// secondary nav menu 
+let allProducts = document.querySelector('#all-products');
+let secondaryNav = document.querySelector('.secondary-nav');
+let secondaryNavMenuPromo = document.querySelector('#promo-gizmo');
+let secondaryNavMenuPhoto = document.querySelector('#photos-videos');
+let secondaryNavMenuItemPromo =document.querySelector('.secondary-nav-menu-item-promo');
+let secondaryNavMenuItemPhoto =document.querySelector('.secondary-nav-menu-item-photo');
+let secondaryNavMenuItemReparateurs = document.querySelector('#reparateur');
+let secondaryNavMenuItemInfo = document.querySelector('#info-contact');
+let closeSecondaryNav = document.querySelector('.close-secondary-menu');
+
+let secondaryNavVisible = () =>{
+    secondaryNav.classList.add('secondary-nav-visible');
+}
+let closeSecondaryNavMenu = () => {
+    secondaryNav.classList.remove('secondary-nav-visible');
+}
+closeSecondaryNav.addEventListener('click', closeSecondaryNavMenu);
+
+allProducts.addEventListener('click', secondaryNavVisible)
+
+let secondaryNavMenuItemPromoVisible = () =>{
+    secondaryNavMenuItemPromo.classList.add('secondary-nav-menu-item-items-visible');
+    secondaryNavMenuItemPhoto.classList.remove('secondary-nav-menu-item-items-visible');
+}
+
+let secondaryNavMenuItemPhotoVisible = () =>{
+    secondaryNavMenuItemPhoto.classList.add('secondary-nav-menu-item-items-visible');
+    secondaryNavMenuItemPromo.classList.remove('secondary-nav-menu-item-items-visible');
+}
+
+let secondaryNavMenuItemPromoInvisible = () =>{
+    secondaryNavMenuItemPromo.classList.remove('secondary-nav-menu-item-items-visible')
+}
+let secondaryNavMenuItemPhotoInvisible = () =>{
+    secondaryNavMenuItemPhoto.classList.remove('secondary-nav-menu-item-items-visible')
+}
+
+let allSecondaryMenuInvisible = () =>{
+    secondaryNavMenuItemPromo.classList.remove('secondary-nav-menu-item-items-visible');
+    secondaryNavMenuItemPhoto.classList.remove('secondary-nav-menu-item-items-visible');
+}
+
+secondaryNavMenuPromo.addEventListener('mouseover', secondaryNavMenuItemPromoVisible);
+
+secondaryNavMenuPhoto.addEventListener('mouseover', secondaryNavMenuItemPhotoVisible);
+
+secondaryNav.addEventListener('mouseleave', closeSecondaryNavMenu);
+
+secondaryNavMenuItemReparateurs.addEventListener('mouseover', allSecondaryMenuInvisible);
+secondaryNavMenuItemInfo.addEventListener('mouseover', allSecondaryMenuInvisible);
